75d575d4d057537d39f1c0cd0a9e8affd747bb50
orderly-guoqing <guoqing@orderly.network>
2026-08-11 21:57:31 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
+  "VITE_DEPLOYMENT_ENV": "mainnet",
   "VITE_ORDERLY_BROKER_ID": "sproutperps",
   "VITE_ORDERLY_BROKER_NAME": "Sprout Perps",
   "VITE_BROKER_EOA_ADDRESS": "0xe8f61bc0037a02e6940e68dbb2668dc5fcc40535",
